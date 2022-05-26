e95d8896f163dc266bade18dbedca1964ccea0ee
devopss1 <abcd@gmail.com>
2022-05-26 12:26:48 -0400

    M1

---
+++ src/main/java/formac/brillius/App.java
@@ -0,0 +1,13 @@
+package formac.brillius;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
